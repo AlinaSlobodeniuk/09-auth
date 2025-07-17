b70b419dd1d91c89521ad245e80278f591b3360d
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-07-18 01:40:42 +0300

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,7 @@ import './globals.css';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { Roboto } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'NoteHub',
@@ -33,6 +24,13 @@ export const metadata: Metadata = {
   },
 };
 
+const roboto = Roboto({
+  subsets: ['latin'], 
+  weight: ['400', '700'],
+  variable: '--font-roboto', 
+  display: 'swap', 
+});
+
 export default function RootLayout({
   children,
   modal,
@@ -42,7 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={roboto.variable}>
         <TanStackProvider>
           <Header />
           {children}
